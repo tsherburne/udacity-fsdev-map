35f1f90000a733682d06a0379bbaa5697f4b8104
Tim Sherburne <sherburne.tim@gmail.com>
2018-02-09 17:36:23 +0000

    Map markers set on map and on sidebar

--- js/items.js
+++ js/items.js
@@ -1,9 +1,20 @@
 /* global ko mapResize */
 
+var locations = [
+    {title: 'UVA Rotunda', location: {lat: 38.0326828, lng: -78.503269}},
+    {title: 'Downtown Mall', location: {lat: 38.029959, lng: -78.4873784}},
+    {title: 'Monticello', location: {lat: 38.0106646, lng: -78.4834914}},
+    {title: "Three Notch'd Brewery", location: {lat: 38.0310149, lng: -78.4838429}},
+    {title: 'Whiskey Jar', location: {lat: 38.0314142, lng: -78.4849477}},
+    {title: 'South Street Brewery', location: {lat: 38.0293422, lng: -78.4848757}}
+];
+    
 var ItemsViewModel = function() {
 
     var self = this;
     self.itemsOpen = ko.observable(false);
+
+    self.mapLocations = locations;
 
     self.openItems = function() {
         self.itemsOpen(true); 
--- js/map.js
+++ js/map.js
@@ -1,24 +1,49 @@
-/* global google */
+/* global google locations */
 var map;
 var uluru = {lat: 38.0400823, lng: -78.5199934};
+
+var markers = [];
 
 
 function initMap() {
 
-
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
-        center: uluru
+        center: uluru,
+        mapTypeControl: false
     });
+    
+ 
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
 
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
+        var marker = new google.maps.Marker({
+                    position: position,
+                    title: title,
+                    animation: google.maps.Animation.DROP,
+                    id: i
+        });
+        markers.push(marker);
+    }
+    showListings();
+}
+
+// This function will loop through the markers array and display them all.
+function showListings() {
+    var bounds = new google.maps.LatLngBounds();
+    // Extend the boundaries of the map for each marker and display the marker
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+        bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
 }
 
 function mapResize() {
-    console.log("Map Resized")
+    console.log("Map Resized");
     google.maps.event.trigger(map, 'resize');
-    map.setCenter(uluru)
+    map.setCenter(uluru);
+    showListings();
 }
